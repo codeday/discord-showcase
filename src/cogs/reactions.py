68d466f4b9ea599f077663301c1de7386a727db1
Bobcat <bobcat4848@gmail.com>
2021-02-20 13:23:16 -0600

    Checkin reactions now report to GQL/Grafana

--- src/cogs/reactions.py
+++ src/cogs/reactions.py
@@ -13,9 +13,7 @@ class Reactions(commands.Cog, name="Reactions"):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        print("on_raw_reaction_added")
         if self.emoji_is_valid(str(payload.emoji)):
-            print("is valid emoji")
             session = session_creator()
 
             pod = PodService.get_pod_by_channel_id(
@@ -26,27 +24,22 @@ class Reactions(commands.Cog, name="Reactions"):
                     int(payload.channel_id))
                 message = await channel.fetch_message(payload.message_id)
                 user_who_posted_message = message.author.id
-                print("made it here1")
                 if user_who_posted_message == self.bot.user.id:
-                    print("made it here2")
-                    print(str(payload.member.id))
                     showcase_user = await GQLService.get_showcase_user_from_discord_id(str(payload.member.id))
                     team_that_reacted = await GQLService.get_showcase_team_by_showcase_user(showcase_user['username'])
-                    print(team_that_reacted)
-                    print(showcase_user)
                     await GQLService.send_team_reacted(str(team_that_reacted[0]['id']), str(showcase_user['username']),
-                                                       float(self.emoji_to_value(payload.emoji)))
+                                                       self.emoji_to_value(str(payload.emoji)))
             session.commit()
             session.close()
 
     def emoji_is_valid(self, emoji):
         return emoji == "😀" or emoji == "😐" or emoji == "☹"
 
-    def emoji_to_value(self, emoji):
+    def emoji_to_value(self, emoji) -> float:
         emoji_values = {
-            "😀": 1,
-            "😐": 0,
-            "☹": -1
+            "😀": 1.0,
+            "😐": 0.0,
+            "☹": -1.0
         }
         return emoji_values.get(emoji)
 
