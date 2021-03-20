e81cb83d8da6807d873fe55b4078ea11ceadaea8
Bobcat <bobcat4848@gmail.com>
2021-03-20 18:10:49 -0500

    Some code commenting and more changes

--- src/converters/PodConverter.py
+++ src/converters/PodConverter.py
@@ -1,14 +1,23 @@
-import discord
-from discord.ext import commands
+from typing import Union
 
+import discord
+
+from db.models import Pod
 from services.podservice import PodService
-from utils.exceptions import PodNameNotFound, PodTCNotFound
+
+"""
+    The purpose of this class will be to sanitize input and return an appropriate pod object from alembic if found.
+"""
 
 
 class PodConverter:
 
     @staticmethod
-    async def get_pod(current_channel: discord.TextChannel, pod_name=None):
+    async def get_pod(current_channel: discord.TextChannel, pod_name=None) -> Union[Pod, None]:
+        """
+        Takes the current channel and an optional pod_name and attempts to find the pod from alembic from
+        one of the arguments. If it finds nothing, it returns None and sends a message to the current_channel.
+        """
         if pod_name is None:
             pod = PodService.get_pod_by_channel_id(str(current_channel.id))
         else:
@@ -16,4 +25,3 @@ class PodConverter:
         if pod is None:
             await current_channel.send("A pod was not able to be found by the text channel or by name.")
         return pod
-
