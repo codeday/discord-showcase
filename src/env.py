80e6cfb6eec4893a7e783640521a06618b2302a9
Bobcat <bobcat4848@gmail.com>
2021-04-01 20:43:16 -0500

    better comments for environment variables

--- src/env.py
+++ src/env.py
@@ -2,19 +2,40 @@ from os import getenv
 
 
 class EnvironmentVariables:
-    # How many teams should be in a singular pod? Change that value here. Default is 5.
+
+    """
+    TEAMS_PER_POD Environment Variable
+    Description: Tells the bot how many teams should be in a singular pod
+    Default Value: 5
+    """
     TEAMS_PER_POD = int(getenv("TEAMS_PER_POD", 5))
 
-    # The role in which the bot will pick a mentor from for each text channel
+    """
+    STAFF_ROLE Environment Variable
+    Description: Tells the bot to add this role to every pod channel
+    Default Value: 689960285926195220, Staff role in the CodeDay Test Server
+    """
     STAFF_ROLE = int(getenv("ROLE_STAFF", 689960285926195220))
 
-    # The role in which the bot will pick a mentor from for each text channel
+    """
+    MENTOR_ROLE Environment Variable
+    Description: Tells the bot which role to pick from when choosing mentors for each pod
+    Default Value: 782363834836975646, Mentor role in the CodeDay Test Server
+    """
     MENTOR_ROLE = int(getenv("ROLE_MENTOR", 782363834836975646))
 
-    # The category in which the pods will reside
+    """
+    CATEGORY Environment Variable
+    Description: Tells the bot which category the pods should reside in
+    Default Value: 783229579732320257, Pods category in CodeDay Test Server
+    """
     CATEGORY = int(getenv("CATEGORY", 783229579732320257))
 
-    # The event ID in which this bot will operate on
+    """
+    EVENT_ID Environment Variable
+    Description: Tells the bot which event to grab teams from in showcase (using gql queries)
+    Default Value: virtual-codeday-winter-2021, latest event
+    """
     EVENT_ID = str(getenv("EVENT_ID", "virtual-codeday-winter-2021"))
 
 
