816ec80fbe76718298091a1da57b8c31e863a55d
Bobcat <bobcat4848@gmail.com>
2021-04-09 12:25:25 -0500

    write pod test cases, there is quite a lot

--- src/cogs/test.py
+++ src/cogs/test.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 
+from cogs.pods import Pods
 from env import EnvironmentVariables
+from finders.mentorfinder import MentorFinder
 from utils import checks
 
 
@@ -21,8 +23,53 @@ class Test(commands.Cog, name="Pods"):
 
     @commands.command(name='test_pods')
     @checks.requires_staff_role()
-    def test_pods(self, ctx: commands.Context):
+    async def test_pods(self, ctx: commands.Context):
+        """Tests all pod commands, requires at least 5 mentors in the given ENV variable role"""
+        guild: discord.Guild = ctx.guild
+        role: discord.Role = guild.get_role(EnvironmentVariables.MENTOR_ROLE)
+        current_channel: discord.TextChannel = ctx.channel
+        test_member: discord.Member = guild.get_member(111572782336208896)  # test member is Jacob Cuomo
 
+        # Create two separate pods with the singular create_pod command
+        await Pods.create_pod(ctx, "DEBUG", MentorFinder.find_a_suitable_mentor(role))
+        await Pods.create_pod(ctx, "DEBUG-2", MentorFinder.find_a_suitable_mentor(role))
+
+        # Create three separate pods with the plural create_pods command
+        await Pods.create_pods(ctx, 3)
+
+        # Assign all created pods up and to this point
+        await Pods.assign_pods(ctx)
+
+        # List teams in a given pod, this command can find teams from current channel, a name, or user
+        await Pods.teams(ctx, "DEBUG")
+        await Pods.teams(ctx, "DEBUG-2")
+        await Pods.teams(ctx, test_member)
+        if "pod" in current_channel.name:
+            await Pods.teams(ctx)
+
+        # Lists all pods in current channel
+        await Pods.pods(ctx)
+
+        # Adds a mentor to a given pod name
+        await Pods.add_mentor(ctx, test_member, "DEBUG")
+        await Pods.add_mentor(ctx, test_member, "DEBUG-2")
+
+        # Merges two pods together, including any teams within them
+        await Pods.merge_pods("DEBUG-2", "DEBUG")
+
+        # Finds a pod and returns their alembic ID
+        await Pods.test(ctx, "DEBUG")
+
+        # Send a message to a pod or all pods
+        await Pods.send_message("DEBUG", "THIS IS JUST A DRILL, DO NOT PANIC!")
+        await Pods.send_message_all("THIS IS JUST A DRILL, DO NOT PANIC!")
+
+        # Lists all teams ever created
+        await Pods.get_all_teams
+
+        # Remove Pod and Remove All Pods
+        await Pods.remove_pod("DEBUG")
+        await Pods.remove_all_pods(ctx)
 
     @commands.command(name='test_checkin')
     @checks.requires_staff_role()
