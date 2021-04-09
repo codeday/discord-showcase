08bf1f6a3edda3e749fabf75f5e934bff51a2087
Bobcat <bobcat4848@gmail.com>
2021-04-09 15:37:20 -0500

    s~teams command works with pod name, discord member, or nothing

--- src/converters/TeamConverter.py
+++ src/converters/TeamConverter.py
@@ -30,41 +30,49 @@ class TeamConverter:
 
     @staticmethod
     async def get_teams(current_channel: discord.TextChannel,
-                        pod_name_or_discord_user: Union[str, discord.User] = None):
+                        pod_name_or_discord_user: str = None):
         print(pod_name_or_discord_user)
-        if pod_name_or_discord_user is None:
+        if pod_name_or_discord_user is None:  # nothing given, get teams from current channel
             print("is none")
             teams = []
             pod = PodDBService.get_pod_by_channel_id(str(current_channel.id), False)
+            await TeamConverter.check_if_teams_exist(teams=pod.teams,
+                                                     output_channel=current_channel,
+                                                     output="There are no projects in your pod yet. Project(s) are still being created by attendee's.")
             for team in pod.teams:
                 showcase_team = await TeamConverter.get_showcase_team_by_id(team.showcase_id)
                 teams.append(showcase_team)
-            if len(teams) == 0 or teams is None:
-                await current_channel.send(
-                    "There are no projects in your pod yet. Project(s) are still being created by attendee's.")
-                raise TeamNotFound()
             return teams
-        elif pod_name_or_discord_user[0] == "<":
+        elif pod_name_or_discord_user[0] == "<":  # discord username given
             filter_out = "<!@>"
             for char in filter_out:
                 pod_name_or_discord_user = pod_name_or_discord_user.replace(char, '')
+            print(pod_name_or_discord_user)
             user = await PodGQLService.get_showcase_user_from_discord_id(str(pod_name_or_discord_user))
+            print(user['username'])
             teams = await PodGQLService.get_showcase_team_by_showcase_user(user['username'])
-            if len(teams) == 0 or teams is None:
-                await current_channel.send(f"<@{pod_name_or_discord_user}> does not belong to any projects.")
-                raise TeamNotFound()
+            print(teams)
+            await TeamConverter.check_if_teams_exist(teams=teams,
+                                                     output_channel=current_channel,
+                                                     output=f"<@{pod_name_or_discord_user}> does not belong to any projects.")
             return teams
-        elif isinstance(pod_name_or_discord_user, str):
+        elif isinstance(pod_name_or_discord_user, str):  # pod name given
             print("is string")
             teams = []
             pod = PodDBService.get_pod_by_name(pod_name_or_discord_user)
+            await TeamConverter.check_if_teams_exist(teams=pod.teams,
+                                                     output_channel=current_channel,
+                                                     output=f"There are no projects in Pod {pod.name} yet. Project(s) are "
+                                                            "still being created by attendee's.")
             for team in pod.teams:
                 showcase_team = await TeamConverter.get_showcase_team_by_id(team.showcase_id)
                 teams.append(showcase_team)
-            if len(teams) == 0 or teams is None:
-                await current_channel.send(
-                    "There are no projects in your pod yet. Project(s) are still being created by attendee's.")
-                raise TeamNotFound()
             return teams
         await current_channel.send("Team(s) were not able to be found by the text channel or by name.")
         raise TeamNotFound()
+
+    @staticmethod
+    async def check_if_teams_exist(teams, output_channel: discord.TextChannel, output: str):
+        if len(teams) == 0 or teams is None:
+            await output_channel.send(output)
+            raise TeamNotFound()
