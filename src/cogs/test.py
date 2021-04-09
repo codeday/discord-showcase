a5ba4e5a6f39070b6b444e9acf0e55dd5d50b3cc
Bobcat <bobcat4848@gmail.com>
2021-04-09 11:35:21 -0500

    add function signatures and annotations

--- src/cogs/test.py
+++ src/cogs/test.py
@@ -1,12 +1,40 @@
 import discord
 from discord.ext import commands
 
+from env import EnvironmentVariables
+from utils import checks
+
 
 class Test(commands.Cog, name="Pods"):
 
-    def __init__(self, bot):
+    def __init__(self, bot: discord.ext.commands.Bot):
         self.bot: discord.ext.commands.Bot = bot
+        self.channel = bot.fetch_channel(EnvironmentVariables.DEBUG_CHANNEL)
 
+    @commands.command(name='test_all')
+    @checks.requires_staff_role()
+    def test_all(self, ctx: commands.Context):
+        pass
+
+    @commands.command(name='test_pods')
+    @checks.requires_staff_role()
+    def test_pods(self, ctx: commands.Context):
+        pass
+
+    @commands.command(name='test_checkin')
+    @checks.requires_staff_role()
+    def test_checkin(self, ctx: commands.Context):
+        pass
+
+    @commands.command(name='test_reactions')
+    @checks.requires_staff_role()
+    def test_reactions(self, ctx: commands.Context):
+        pass
+
+    @commands.command(name='test_listeners')
+    @checks.requires_staff_role()
+    def test_listeners(self, ctx: commands.Context):
+        pass
 
 
 def setup(bot):
